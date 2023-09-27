e436e6e357ee9a44ce8e89c24ce4d0653f20a1d6
Seunghyeok Kim <shkim8745@gmail.com>
2023-09-26 17:50:29 -0700

    git push all the files to my repository

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $('#lightSlider').lightSlider({
+    item: 3, //
+    autoWidth: false,
+    slideMove: 1,
+    slideMargin: 10,
+    addClass: '',
+    mode: 'slide',
+    useCSS: true,
+  });
+});
